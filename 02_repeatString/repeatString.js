8b5cb78dacfebc800a07d2d53ad664eac6964246
NduNdu21 <ndumisobm@icloud.com>
2025-01-17 10:57:39 +0000

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let display = ""
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++){
+            display += string
+        }
+        return display
+    }
 };
 
 // Do not edit below this line
