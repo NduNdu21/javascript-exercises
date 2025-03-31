a3733ebf33f20a3ed20cc4c6067a06cbe34a288f
NduNdu21 <ndumisobm@icloud.com>
2025-03-31 15:17:05 +0100

    Completed exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (pos) {
+
+    if (pos < 0) {
+        return "OOPS"
+    } else {
+        let value = 1
+
+        // initialising fib array with first 4 values
+        let fibArr = [0, 1]
+
+        //get value of position
+        for (let i = 2; i <= pos; i++) {
+            fibArr[i] = fibArr[i - 1] + fibArr[i - 2]
+        }
+
+        value = fibArr[pos]
+
+        return value
+    }
 
 };
 
