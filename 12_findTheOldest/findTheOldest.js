aa2a896614369e88883ff17ccabd1ea2d675db3c
NduNdu21 <ndumisobm@icloud.com>
2025-04-03 18:09:24 +0100

    Completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let oldest = peopleArr[0]
 
+    peopleArr.forEach(
+        (person) => {
+            if (getAge(person) > getAge(oldest)) {
+                oldest = person
+            }
+        }
+    )
+
+    return oldest
 };
+
+const getAge = function(person) {
+    if (person.yearOfDeath != null) {
+        return person.yearOfDeath - person.yearOfBirth
+    } else {
+        //create Date object and then get year
+        var today = new Date()
+        var year = today.getFullYear()
+
+        return year - person.yearOfBirth
+    }
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
